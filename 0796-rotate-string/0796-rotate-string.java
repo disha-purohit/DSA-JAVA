9246dbe6b9450d573d1e834d458e16f21d68d95f
Disha Purohit <dishaprht123@gmail.com>
2026-08-21 22:27:12 +0530

    Time: 2 ms (63.49%), Space: 43.6 MB (31.36%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -4,11 +4,9 @@ class Solution {
         if (s.length()  != goal.length()){
             return false;
         }
-        for(int i = 0 ; i < s.length(); i++){
-            String rotated = s.substring(i) +s.substring (0, i);
-            if(rotated.equals(goal)){
-                return true;
-            }
+        String doubles = s + s;
+        if(doubles.contains(goal)){
+            return true;
         }
         return false;
     }
