09552d543aa5d34baab69ca9b14bc55f6f820f99
Disha Purohit <dishaprht123@gmail.com>
2026-09-25 22:54:29 +0530

    Time: 2 ms (89.62%), Space: 47.7 MB (90.84%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max_index = 0;
+        for(int i = 0 ;  i < nums.length ; i++){
+            if( i > max_index){
+                return false;
+            }
+            max_index = Math.max(max_index , i + nums[i]);
+        }
+        return true;
+    }
+}
